db80a46a0a6a34958eefc62d85816006dae71406
Gusabary <gusabary@126.com>
2020-12-29 21:17:32 +0800

    feat: init common lib

---
+++ include/uno/msg_builder.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <core/provider.pb.h>
+#include <core/error.pb.h>
+#include <uno/uno.pb.h>
+
+#include "card.h"
+#include "../common/types.h"
+#include "../common/util.h"
+
+namespace SailGame { namespace Game {
+
+using ::Uno::GameStart;
+using ::Uno::NotifyMsg;
+using ::Uno::StartGameSettings;
+using ::Uno::Draw;
+using ::Uno::Skip;
+using ::Uno::Play;
+using Core::ErrorNumber;
+using Core::NotifyMsgArgs;
+using Core::ProviderMsg;
+using Common::ProviderMsgPtr;
+using Common::Util;
+
+class MsgBuilder {
+public:
+    static ProviderMsgPtr CreateRegisterArgs(int seqId, const std::string &id,
+        const std::string &gameName, int maxUsers, int minUsers);
+
+    static ProviderMsgPtr CreateRegisterRet(int seqId, ErrorNumber err);
+
+    static ProviderMsgPtr CreateStartGameArgs(int seqId, int roomId, 
+        const std::vector<unsigned int> userIds, const StartGameSettings &custom);
+    
+    static StartGameSettings CreateStartGameSettings(bool isDraw2Consumed,
+        bool canSkipRespond, bool hasWildSwapHandsCard, bool canDoubtDraw4, int roundTime);
+
+    static ProviderMsgPtr CreateNotifyMsgArgs(int seqId, ErrorNumber err, 
+        int roomId, int userId, const NotifyMsg &custom);
+
+    static NotifyMsg CreateGameStart(const InitHandcardsT &initHandcards, 
+        Card flippedCard, int firstPlayer);
+
+    static NotifyMsg CreateDraw(const Draw &draw);
+
+    static NotifyMsg CreateSkip(const Skip &skip);
+  
+    static NotifyMsg CreatePlay(const Play &play);
+  
+    static NotifyMsg CreateDrawRsp(const std::vector<Card> &cards);
+};
+}}
